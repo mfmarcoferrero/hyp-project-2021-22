c01b9a086acf0de409c43c0828d7937f73293cf5
Vega98 <vegastick.98@gmail.com>
2022-04-27 18:39:51 +0200

    added carousel

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -16,7 +16,7 @@
       <!-- Text slides with image -->
       <b-carousel-slide
         caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        text="Porcodio"
         img-src="https://picsum.photos/1024/480/?image=52"
       ></b-carousel-slide>
 
@@ -56,6 +56,7 @@
 
 <script>
   export default {
+    name: 'Carousel',
     data() {
       return {
         slide: 0,
