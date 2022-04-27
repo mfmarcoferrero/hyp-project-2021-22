c1953b250ec65ec09ea510c7fc3f01689b229fe5
Marco Ferrero <marcoadriano.ferrero@mail.polimi.it>
2022-04-28 01:14:19 +0200

    server packages added

--- server/api.js
+++ server/api.js
@@ -9,7 +9,6 @@ const database = new Sequelize("postgres://postgres:postgres@localhost:5432/hyp"
 // Function that will initialize the connection to the database
 async function initializeDatabaseConnection() {
     await database.authenticate()
-
     await database.sync({ force: true })
     return {
         
