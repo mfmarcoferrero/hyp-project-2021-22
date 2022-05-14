5ccdbf9f39bef55e54ebfb88492685e145d71dab
Marco Ferrero <marcoadriano.ferrero@mail.polimi.it>
2022-05-14 16:01:22 +0200

    nuxt-link in overlay card

--- components/OverlayCard.vue
+++ components/OverlayCard.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="card shadow-lg p-0 mb-5 text-white">
-    <img :src="img" class="card-img" alt="No image found">
-    <div class="card-img-overlay">
-      <h3 class="card-title text-center">{{ name }}</h3>
-      <p class="card-text text-center">{{ description }}</p>
+  <nuxt-link :to="`/details/${id}`">
+    <div class="card shadow-lg p-0 mb-5 text-white" @OnClick="goToDetails()">
+      <img :src="img" class="card-img" alt="No image found">
+      <div class="card-img-overlay">
+        <h3 class="card-title text-center">{{ name }}</h3>
+        <p class="card-text text-center">{{ description }}</p>
+      </div>
     </div>
-  </div>
+  </nuxt-link>
 </template>
 
 
@@ -28,11 +30,6 @@ export default {
     description: {
       type: String,
       required: true,
-    },
-  },
-  methods: {
-    goToDetails() {
-      this.$router.push(`/details/${this.id}`)
     },
   },
 }
