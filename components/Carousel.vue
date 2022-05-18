718847299dc80f2172bb3cb6c3d1d60943c0173d
Marco Ferrero <marcoadriano.ferrero@mail.polimi.it>
2022-05-18 15:36:21 +0200

    on click carousel

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,69 +1,6 @@
-<!-- <template>
-  <div>
-    <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      img-width="1024px"
-      img-height="480px"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-      <div>
-        <b-carousel-slide
-          v-for="(detail, detailIndex) of detailList"
-          :key="`detail-index-${detailIndex}`"
-          :caption="detail.name"
-        >
-          <b-img
-            slot="img"
-            class="d-block img-fluid w-100"
-            img-width="1024px"
-            img-height="480px"
-            :src="detail.img"
-            alt="image slot"
-            v-on:click="goTo(detail.id)"
-          />
-        </b-carousel-slide>
-      </div>
-    </b-carousel>
-  </div>
-</template>
-
-<script>
-// TODO: Fix the img size (even if images on db have different sizes) and change the cursor icon when on the slide
-
-export default {
-  name: 'Carousel',
-  props: {
-    detailList: Array,
-    required: true,
-  },
-  data() {
-    return {
-      slide: 0,
-      sliding: null,
-    }
-  },
-  methods: {
-    onSlideStart(slide) {
-      this.sliding = true
-    },
-    onSlideEnd(slide) {
-      this.sliding = false
-    },
-    goTo(id) {
-      this.$router.push(`/details/${id}`)
-    },
-  },
-}
-</script> -->
-
 <template>
   <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
+    <div class="carousel-inner" >
 
       <div class="carousel-item active">
         <img src="@/static/pictures/dam.jpg" class="d-block w-100" alt="first">
@@ -73,7 +10,7 @@ export default {
         </div>
       </div>
 
-      <div v-for="(detail, detailIndex) of detailList" :key="`detail-index-${detailIndex}`" class="carousel-item">
+      <div v-for="(detail, detailIndex) of detailList" :key="`detail-index-${detailIndex}`" class="carousel-item" v-on:click="goTo(detail.id)">
         <img :src="detail.img" class="d-block w-100" alt="second">
         <div class="carousel-caption d-none d-md-block">
           <h2>{{detail.name}}</h2>
