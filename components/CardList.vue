de203ef65462d474950b9ba14f5272af68afcc7b
Vega98 <vegastick.98@gmail.com>
2022-06-10 00:53:52 +0200

    started experimenting with conditional rendering

---
+++ components/CardList.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="row">
+    <div class="col-4">
+      <div class="list-group sticky" id="list-tab" role="tablist">
+        <a
+          class="list-group-item list-group-item-action active"
+          id="list-winter-list"
+          data-bs-toggle="list"
+          href="#list-winter"
+          role="tab"
+          aria-controls="list-winter"
+          @click="season = `winter`"
+          >Winter events</a
+        >
+        <a
+          class="list-group-item list-group-item-action"
+          id="list-spring-list"
+          data-bs-toggle="list"
+          href="#list-spring"
+          role="tab"
+          aria-controls="list-spring"
+          @click="season = `spring`"
+          >Spring events</a
+        >
+        <a
+          class="list-group-item list-group-item-action"
+          id="list-summer-list"
+          data-bs-toggle="list"
+          href="#list-summer"
+          role="tab"
+          aria-controls="list-summer"
+          @click="season = `summer`"
+          >Summer events</a
+        >
+        <a
+          class="list-group-item list-group-item-action"
+          id="list-fall-list"
+          data-bs-toggle="list"
+          href="#list-fall"
+          role="tab"
+          aria-controls="list-fall"
+          @click="season = `fall`"
+          >Fall events</a
+        >
+      </div>
+    </div>
+    <div class="col-8">
+      <div class="tab-content" id="nav-tabContent">
+        <div
+          v-for="(event, eventIndex) of list"
+          :key="eventIndex"
+          class="tab-pane fade"
+          :id="`list-${event.season}`"
+          role="tabpanel"
+          :aria-labelledby="`list-${event.season}-list`"
+        >
+          <h2>
+            <nuxt-link :to="`/events/test`">
+              {{ event.name }}
+            </nuxt-link>
+          </h2>
+          <p>
+            {{ event.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardList',
+  props: {
+    list: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    filteredItems(){
+    return this.list.filter(season => this.selectedCategories.includes(season))
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
--- pages/events/index.vue
+++ pages/events/index.vue
@@ -26,17 +26,6 @@
     </div>
   </div>
   </div> -->
-  <div class="row">
-  <div class="col-4">
-    <div class="list-group sticky" id="list-tab" role="tablist">
-      <a class="list-group-item list-group-item-action active" id="list-winter-list" data-bs-toggle="list" href="#list-winter" role="tab" aria-controls="list-winter">Winter events</a>
-      <a class="list-group-item list-group-item-action" id="list-spring-list" data-bs-toggle="list" href="#list-spring" role="tab" aria-controls="list-spring">Spring events</a>
-      <a class="list-group-item list-group-item-action" id="list-summer-list" data-bs-toggle="list" href="#list-summer" role="tab" aria-controls="list-summer">Summer events</a>
-      <a class="list-group-item list-group-item-action" id="list-fall-list" data-bs-toggle="list" href="#list-fall" role="tab" aria-controls="list-fall">Fall events</a>
-    </div>
-  </div>
-  <div class="col-8">
-    <div class="tab-content" id="nav-tabContent">
 <!--       <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
         <h4>Winter events</h4>
         <p>{{lipsum}}</p>
@@ -53,35 +42,20 @@
         <h4>Autumn events</h4>
         <p>{{lipsum}}</p>
       </div> -->
-      <div v-for="(event, eventIndex) of eventList" 
-           :key="eventIndex" 
-           class="tab-pane fade"
-           :class={show: eventIndex == 0}
-           :class={active: eventIndex == 0}
-           :id="`list-${event.season}`"
-           role="tabpanel"
-           :aria-labelledby="`list-${event.season}-list`">
-      <h2>
-        <nuxt-link :to="`/events/test`">
-          test
-        </nuxt-link>
-      </h2>
-      <p>
-        {{ event.description }}
-      </p>
-    </div>
-  </div>
-</div>
+      
+      <card-list :list="eventList" />
 
   </div>
 </template>
 
 <script>
 import Map from '@/components/Map.vue'
+import CardList from '@/components/CardList.vue'
 export default {
   
   components: {
     Map,
+    CardList,
   },
   data() {
     return {
