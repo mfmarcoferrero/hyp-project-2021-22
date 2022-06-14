b950eabdcd723d0346326038ff26fb2ae005a1da
Marco Ferrero <marcoadriano.ferrero@mail.polimi.it>
2022-06-14 11:40:55 +0200

    cover image component

---
+++ components/CoverImage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <img :src="img" alt="no image" class="main-img">
+        <h1 class="title"> {{ title }} </h1>
+    </div>
+</template>
+
+<script>
+import CommonMixin from '~/mixins/common';
+
+export default {
+    name: 'CoverImage',
+    mixins: [CommonMixin],
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        img: {
+            type: String,
+            required: true,
+        }
+    },
+}
+</script>
+
+<style>
+.main-img {
+    width: 100%;
+    height: 500px;
+    object-fit: cover;
+}
+</style>
