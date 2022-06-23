512291dfbc9eee5b0647c7ce18f670d0b0c112f7
Vega98 <vegastick.98@gmail.com>
2022-06-23 19:25:50 +0200

    styling

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -3,9 +3,10 @@
     <cover-image :img="coverImg" :title="title" />
 
     <div class="row m-5">
-      <h1 style="text-align: center">What's up in Amsterdam?</h1>
+      <h1 style="text-align: center"><strong>What's up in Amsterdam?</strong></h1>
+      <h3 style="text-align: center">Choose a time period!</h3>
     </div>
-    <card-list :list="eventList" />
+    <card-list :list="eventList" :categories="categories" />
   </div>
 </template>
 
@@ -23,7 +24,8 @@ export default {
   data() {
     return {
       coverImg: "https://s8.gifyu.com/images/events-cover.jpg",
-      title: "Festivals & Events"
+      title: "Festivals & Events",
+      categories: ["all", "winter", "spring", "summer", "fall"]
     }
   },
   async asyncData({ $axios }) {
--- pages/services/index.vue
+++ pages/services/index.vue
@@ -4,46 +4,89 @@
     <!-- v-for bootstrap cards taking elements from database -->
     <div class="container position-relative pb-5">
       <div class="row mt-3 justify-content-md-center">
-        <hover-card v-for="(item, itemIndex) of serviceDetails" class="col-sm-auto m-2" :key="`data-index-${itemIndex}`"
-          :id="item.id" :name="item.name" :img="item.img" :description="item.description"
-          :path="'services/' + item.name" />
+        <section id="topic-menu">
+          <div class="container">
+            <br>
+            <h2 class="text-center"><strong>Which kind of service?</strong></h2>
+            <div class="row m-5">
+              <div class="col-md m-2">
+                <topic-card
+                  title="Medical Services"
+                  img="https://s8.gifyu.com/images/808x454_cmsv2_b3d598a0-bec1-5f13-bc92-88ae94163b73-6744142.jpg"
+                  path="services/Medical_Services"
+                />
+              </div>
+              <div class="col-md m-2">
+                <topic-card
+                  title="Cultural Sergives"
+                  img="https://s8.gifyu.com/images/91a051430ab15c667bad0d64bba0ff45299d9f28.jpg"
+                  path="services/Cultural_Services"
+                />
+              </div>
+              <div class="col-md m-2">
+                <topic-card
+                  title="Leisure Services"
+                  img="https://s8.gifyu.com/images/14896f158a325e49575.jpg"
+                  path="services/Thematic_and_leisure_parks"
+                />
+              </div>
+              <div class="col-md m-2">
+                <topic-card
+                  title="Work Services"
+                  img="https://s8.gifyu.com/images/808x454_cmsv2_b3d598a0-bec1-5f13-bc92-88ae94163b73-6744142.jpg"
+                  path="services/Job_Services"
+                />
+              </div>
+              <div class="col-md m-2">
+                <topic-card
+                  title="Student Services"
+                  img="https://s8.gifyu.com/images/808x454_cmsv2_b3d598a0-bec1-5f13-bc92-88ae94163b73-6744142.jpg"
+                  path="services/Student_Services"
+                />
+              </div>
+            </div><br><br><br>
+            <div class="text-center">
+            <img src="https://i.pinimg.com/originals/f2/50/95/f250958979ba717571bf069ea43f053c.gif"
+                   class="img-responsive text-center"
+                   style="height:150px; width:200px;" >
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 import Card from '@/components/Card.vue'
 import Masonry from '@/components/Masonry.vue'
 import HoverCard from '@/components/HoverCard.vue'
 import CoverImage from '~/components/CoverImage.vue'
 
 export default {
-
   components: {
     Card,
     Masonry,
     HoverCard,
-    CoverImage
+    CoverImage,
   },
 
   data() {
     return {
-      coverImg: "https://s8.gifyu.com/images/netherlands-transportation-1920x1080.jpg",
-      title: "Services & Facilities"
+      coverImg:
+        'https://s8.gifyu.com/images/netherlands-transportation-1920x1080.jpg',
+      title: 'Services & Facilities',
     }
   },
 
   async asyncData({ $axios }) {
-    // Database table to populate service image, service description and markers array to show on map  
+    // Database table to populate service image, service description and markers array to show on map
     const { data } = await $axios.get('/api/services')
     return {
       serviceDetails: data,
-      path: "services"
+      path: 'services',
     }
-  }
-
+  },
 }
 </script>
 
@@ -51,6 +94,7 @@ export default {
 .wrapper {
   overflow: visible;
   margin: 0 auto 100px auto;
+  background:white;
 }
 
 .cardwrapper {
