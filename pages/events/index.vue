ae82ae8c2dc6e08be5e23070a77d8f1cdccfe510
Vega98 <vegastick.98@gmail.com>
2022-06-24 21:34:44 +0200

    stashing to check other branch

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -2,12 +2,28 @@
   <div class="page-container wrapper">
     <cover-image :img="coverImg" :title="title" />
 
+  <section id="events-by-season">
     <div class="row m-5">
       <h1 style="text-align: center"><strong>What's up in Amsterdam?</strong></h1>
+      <p class="second-title vl ms-5 mt-5 mb-4">Almost every week a cultural event or festival is held in Amsterdam. Many of these Amsterdam events and
+          festivals have a long tradition and are carefully conserved by the Dutch. But these typical Dutch festivities
+          are also very nice for tourists, because a lot of them are held with an English-speaking public in mind.</p>
       <h3 style="text-align: center">Choose a time period!</h3>
     </div>
     <card-list :list="eventList" :categories="categories" />
+  </section>
+
+  <hr class="m-5">
+
+  <section id="agenda">
+    <div class="container ps-5">
+    <h2 class="second-title v1 mt-5">Cultural Agenda</h2>
+    <agenda :list="eventList" class="mt-5" />
+    </div>
+  </section>
+
   </div>
+  
 </template>
 
 <script>
